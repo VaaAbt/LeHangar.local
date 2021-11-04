97453a936465cc7a6283a1184d6182184aaabef3
VaaAbt <valentinaubertin@outlook.fr>
2021-11-04 09:28:33 +0100

    refactor: deleting useless lines

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
-//session_start();
 
-//use App\Middleware\CORSMiddleware;
-//use Slim\Csrf\Guard;
 use DI\Container;
 use Slim\Factory\AppFactory;
 
@@ -12,24 +9,8 @@ $container = new Container();
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-/**$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-$responseFactory = $app->getResponseFactory();
-
-// Register Middleware On Container
-$container->set('csrf', function () use ($responseFactory) {
-    return new Guard($responseFactory);
-});
-
-// Register Middleware To Be Executed On All Routes
-$app->add('csrf');
-
-// CORS
-$app->add(new CORSMiddleware());**/
-
 require __DIR__ . '/../conf/dependencies.php';
 require __DIR__ . '/../conf/bootstrap.php';
-
 require __DIR__ . '/../src/routes.php';
 
 $app->run();
