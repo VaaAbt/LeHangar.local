4744e7687936da6b6ead9cf9b7c0c94d8c6b3848
VaaAbt <valentinaubertin@outlook.fr>
2021-11-04 14:34:04 +0100

    feat: user can add and remove quantity of a product

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -11,9 +11,10 @@ class CartController extends AbstractController
     public function getCartView(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $products = array();
+
         if(isset($_SESSION['cart'])){
             for ($i=0; $i < count($_SESSION['cart']); $i++) { 
-                array_push($products, Product::getProductById($_SESSION['cart'][$i]));
+                array_push($products, [Product::getProductById($_SESSION['cart'][$i]), $_SESSION['cart'][$i][1]]);
             }
         }
         return $this->render($response, 'cart.html.twig', [
--- src/routes.php
+++ src/routes.php
@@ -18,4 +18,8 @@ $app->get('/', [HomeController::class, 'index']);
 $app->get('/products', [ProductController::class, 'getAllProducts']);
 $app->post('/products/addToCart/{id}', [ProductController::class, 'addProductToCart']);
 
+$app->get('/products/addToCart/{id}/addOne', [ProductController::class, 'addProductQuantityToCart']);
+
+$app->get('/products/addToCart/{id}/removeOne', [ProductController::class, 'removeProductQuantityToCart']);
+
 $app->get('/cart', [CartController::class, 'getCartView']);
