a7563553c7b8b380c3bf2f490e383c1fff73e206
Orias Theo <orias.theo@gmail.com>
2021-11-05 17:11:46 +0100

    fonction+ - des ajout au panier corriger

--- public/js/products.js
+++ public/js/products.js
@@ -21,20 +21,14 @@ search.addEventListener('input', () => {
     }
 });
 
-function incrementValue()
+function incrementValue(val)
 {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
+    document.getElementById('number'+val).value++;
 }
 
-function decrementValue()
+function decrementValue(val)
 {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    if(value > 1){
-        value--;
+    if(document.getElementById('number'+val).value > 1){
+        document.getElementById('number'+val).value--;
     }
-    document.getElementById('number').value = value;
 }
