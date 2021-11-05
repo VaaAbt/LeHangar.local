52a64cb5b7fd211834fe9e8b3a420032bea0c86c
Vibeso <TheAlexdu55@gmail.com>
2021-11-05 14:04:00 +0100

    fix: GrowerController

--- src/Controller/GrowerController.php
+++ src/Controller/GrowerController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Model\Grower;
-use App\Model\Product;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -12,15 +11,15 @@ class GrowerController extends AbstractController
     public function getAllGrowers(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $growers = Grower::getAll();
-        return $this->render($response, 'products/products.html.twig', [
-            'products' => $growers
+        return $this->render($response, 'grower.html.twig', [
+            'growers' => $growers
         ]);
     }
 
     public function getGrowerById(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $grower = Grower::getGrowerById($args['id']);
-        $products = Product::getProductsById_Grower($grower->id,3);
+        $products = Product::getProductsById_Grower($grower->id, 3);
 
         return $this->render($response, 'detailedGrower.html.twig', [
             'grower' => $grower,
