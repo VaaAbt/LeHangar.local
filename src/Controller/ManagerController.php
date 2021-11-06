3e5aa4ff236e22c705870506c62d47a3e3d6a0fb
leopold <leopold@pop-os.localdomain>
2021-11-06 11:26:14 +0100

    Orders manager view with error 'Cannot render the view'

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Model\listProducts;
 use App\Model\Manager;
 use App\Model\Order;
+use App\Model\Product;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -21,6 +23,50 @@ class ManagerController extends AbstractController
             'nb_ordSend' => $orders_send->count(),
             'nb_ordVal' => $orders_validate->count()
         ]);
+    }
+
+
+    public function ordersView(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $orders_pending = []; $orders_sending = []; $orders_validate = [];
+
+        $tmp_Pend = Order::getPending();
+        $tmp_Send = Order::getSend();
+        $tmp_Val = Order::getValidate();
+
+        foreach($tmp_Pend as $pend){
+            $orders_pending[$pend->getAttribute('id')][] = $pend;
+
+            $list = listProducts::query()->where('id_order', $pend->getAttribute('id'))->get();
+            foreach($list as $l){
+                $orders_pending[$pend->getAttribute('id')][] = Product::getProductById($l->getAttribute('id_product'));
+            }
+        }
+
+        foreach($tmp_Send as $send){
+            $orders_sending[$send->getAttribute('id')][] = $send;
+
+            $list = listProducts::query()->where('id_order', $send->getAttribute('id'))->get();
+            foreach($list as $l){
+                $orders_sending[$send->getAttribute('id')][] = Product::getProductById($l->getAttribute('id_product'));
+            }
+        }
+
+        foreach($tmp_Val as $val){
+            $orders_validate[$val->getAttribute('id')][] = $val;
+
+            $list = listProducts::query()->where('id_order', $val->getAttribute('id'))->get();
+            foreach($list as $l){
+                $orders_validate[$val->getAttribute('id')][] = Product::getProductById($l->getAttribute('id_product'));
+            }
+        }
+
+        return $this->render($response, 'account/manager_orders.html.twig', [
+            'ordPend' => $orders_pending,
+            'ordSend' => $orders_sending,
+            'ordVal' => $orders_validate
+        ]);
+
     }
 
 }
